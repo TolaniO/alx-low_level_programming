18ef18fae0b7830e92c606fd04c87b1513aab755
TolaniO <aramidemide@outlook.com>
2023-04-25 16:40:20 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit
- * of a number
+ * of a number.
  *
  * @n: input number as an integer.
  *
@@ -10,7 +10,7 @@
  */
 int print_last_digit(int n)
 {
-	int l,
+	int l;
 
 	l = n % 10;
 	if (l < 0)
