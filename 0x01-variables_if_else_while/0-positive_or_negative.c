ea3ccee62307e5d5f5dffad1dcb369662b649801
TolaniO <aramidemide@outlook.com>
2023-04-24 17:07:39 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 
+
 /**
  * main - Prints if number is positive, zero or negative 
  * Return: Always (Success)
@@ -12,6 +13,7 @@
 
 int main(void)
 {
+
 	int n;
 
 	srand(time(0));
