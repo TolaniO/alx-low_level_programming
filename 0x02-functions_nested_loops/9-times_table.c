3d157b44e8670d61e1ed12d662ecd05b2f6dc42a
TolaniO <aramidemide@outlook.com>
2023-04-25 18:51:37 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,8 +18,8 @@ void times_table(void)
 			if ((n / 10) == 0)
 			{
 				if (j != 0)
-				_putchar(' ');
-				_putchar(n + 0);
+					_putchar(' ');
+				_putchar(n + '0');
 
 				if (j == 9)
 					continue;
