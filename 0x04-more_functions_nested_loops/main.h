d23c651c29d2bfd64af563ba0c1bc79c0b3e28f7
TolaniO <aramidemide@outlook.com>
2023-04-27 19:09:02 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul (int a. int b);
+void print_numbers(void);
+int _putchar(char);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int n);
+void print_triangle(int n);
+void print_number(int n);
+
+#endif
